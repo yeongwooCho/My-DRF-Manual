869d5ec3a8a0ecd4b193cdf6e86f76b57f082bff
yeongwoo <jyy0223@naver.com>
2023-06-14 19:02:12 +0900

    [add] startapp blog and api

--- total_env/settings.py
+++ total_env/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # my app
+    'api.apps.ApiConfig',
+    'blog.apps.BlogConfig'
 
     # third party package
     'rest_framework',
