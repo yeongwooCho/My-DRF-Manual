613f34b48852c6836fd621e3842ceceb0ca1afee
yeongwoo <jyy0223@naver.com>
2023-06-14 21:10:48 +0900

    [add] 예제코드 및 파일 추가

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class PostDetailTV(TemplateView):
+    template_name = 'blog/post_detail.html'
--- total_env/urls.py
+++ total_env/urls.py
@@ -24,11 +24,19 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from total_env.views import HomeView
+
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 
+    # blog app example code
+    path('', HomeView.as_view(), name='home'),
+    path('blog/', include('blog.urls')),
+    # path('api/', include('api.urls')),
+
+    # api app: 이전 api의 기능들을 동일하게 동작하되 DRF로 만든 API
     path('api/', include('api.urls')),
 ]
