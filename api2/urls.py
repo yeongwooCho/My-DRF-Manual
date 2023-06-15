705b8f6458234034a46060f0f818876bba9c4b94
yeongwoo <jyy0223@naver.com>
2023-06-15 17:00:28 +0900

    [add] ViewSet을 Generic Views 로 변경

--- api2/urls.py
+++ api2/urls.py
@@ -1,16 +1,28 @@
-from django.urls import path, include
-from rest_framework import routers
+# from django.urls import path, include
+# from rest_framework import routers
+#
+# from api2.views import UserViewSet, PostViewSet, CommentViewSet
+#
+# # Routers provide an easy way of automatically determining the URL conf.
+# router = routers.DefaultRouter()
+# router.register(r'users', UserViewSet)
+# router.register(r'posts', PostViewSet)
+# router.register(r'comments', CommentViewSet)
+#
+# # Wire up our API using automatic URL routing.
+# # Additionally, we include login URLs for the browsable API.
+# urlpatterns = [
+#     path('', include(router.urls)),
+# ]
 
-from api2.views import UserViewSet, PostViewSet, CommentViewSet
+from django.urls import path
+from api2 import views
 
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-router.register(r'posts', PostViewSet)
-router.register(r'comments', CommentViewSet)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
+    # posts
+    path('posts/', views.PostListAPIView.as_view(), name='post-list'),
+    path('posts/<int:pk>/', views.PostRetrieveAPIView.as_view(), name='post-detail'),
+
+    # comments
+    path('comments/', views.CommentCreateAPIView.as_view(), name='comments-list'),
 ]
