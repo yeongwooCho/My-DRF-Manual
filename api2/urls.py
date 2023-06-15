98b94950d7b9ef0a8d683b5c423bea2c4f66ddce
yeongwoo <jyy0223@naver.com>
2023-06-15 15:20:06 +0900

    [add] post api

--- api2/urls.py
+++ api2/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from api2.views import UserViewSet
+from api2.views import UserViewSet, PostViewSet
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
+router.register(r'posts', PostViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
--- api2/views.py
+++ api2/views.py
@@ -1,9 +1,17 @@
 from django.contrib.auth.models import User
+from blog.models import Post
 from rest_framework import viewsets
 
-from api2.serializers import UserSerializer
+from api2.serializers import UserSerializer, PostSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
